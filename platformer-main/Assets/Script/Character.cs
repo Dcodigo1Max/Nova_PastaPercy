2e4f42842bd9fb15f8b6a2650bec935c2c9f01e3
bernabarros <bernardobarros.work@gmail.com>
2025-05-09 02:03:23 +0100

    Change Player and Enemy scripts, Create Character Script and add code, remove friction from Tileset, Create Defeault_Friction Material

---
+++ platformer-main/Assets/Script/Character.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    [SerializeField]
+    protected Vector2 velocity;
+    [SerializeField]
+    protected Transform groundCheck;
+    [SerializeField, Range(0.1f, 5.0f)]
+    protected float groundCheckRadius = 2.0f;
+    [SerializeField]
+    protected LayerMask groundCheckLayers;
+    protected Rigidbody2D rb;
+    protected SpriteRenderer spriteRenderer;
+    protected Animator animator;
+    protected Quaternion initialRotation;
+    protected bool isGround;
+    protected abstract float GetDirection();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected virtual void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        initialRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if ((GetDirection() < 0) && (transform.right.x > 0))
+        {
+            transform.rotation = initialRotation * Quaternion.Euler(0.0f, 180.0f, 0.0f);
+        }
+        else if ((GetDirection() > 0) && (transform.right.x < 0))
+        {
+            transform.rotation = initialRotation;
+        }
+
+        Vector2 currentVelocity = rb.linearVelocity;
+
+        animator.SetFloat("AbsVelocity", Mathf.Abs(currentVelocity.x));
+        animator.SetFloat("VelocityY", currentVelocity.y);
+        animator.SetBool("isGrounded", isGround);
+    }
+
+    protected void ComputeGrounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundCheckLayers);
+
+        if(collider != null)
+        {
+            isGround = true;
+        }
+        else
+        {
+            isGround = false;
+        }
+    }
+    protected virtual void OnDrawGizmosSelected()
+    {
+        if(groundCheck == null) return;
+
+        ComputeGrounded();
+
+        if (isGround) Gizmos.color = Color.green;
+        else Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
