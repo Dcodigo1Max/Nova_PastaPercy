5a560b5fbb8d3e7fbbe7e37800ddbbc550f59897
bernabarros <bernardobarros.work@gmail.com>
2025-05-13 23:05:36 +0100

    Add WaterPower restore code

--- platformer-main/Assets/Script/Player.cs
+++ platformer-main/Assets/Script/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : Character
@@ -49,6 +50,7 @@ public class Player : Character
     protected override void Update()
     {
         ComputeGrounded();
+
         groundCollider.enabled = isGround;
         airCollider.enabled = !isGround;
 
--- platformer-main/Assets/Script/WaterSystem.cs
+++ platformer-main/Assets/Script/WaterSystem.cs
@@ -1,13 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class WaterSystem : MonoBehaviour
 {
     [SerializeField]
     private float maxWaterPower = 100.0f;
-    
+
     public float waterpower;
     public float wp => waterpower;
     public float wpNormalized => wp / maxWaterPower;
+
+    [SerializeField]
+    private Transform waterCheck;
+    [SerializeField, Range(0.1f, 5.0f)]
+    private float waterCheckRadius = 2.0f;
+    [SerializeField]
+    private LayerMask waterCheckLayers;
+    private bool inWater;
+
 
     void Awake()
     {
@@ -16,7 +26,12 @@ public class WaterSystem : MonoBehaviour
 
     void Update()
     {
-        
+        ComputeWater();
+        if(inWater)
+        {
+            StartCoroutine(RestoreCooldown());
+            //InvokeRepeating(nameof(IncreaseWaterPower),1f,1f);
+        }
     }
 
     public bool ReduceWaterPower()
@@ -30,5 +45,26 @@ public class WaterSystem : MonoBehaviour
         if(waterpower >= maxWaterPower) return false;
         waterpower++;
         return true;
+    }
+
+    void ComputeWater()
+    {
+        Collider2D watercollider = Physics2D.OverlapCircle(waterCheck.position, 
+        waterCheckRadius, waterCheckLayers);
+
+        if ( watercollider != null)
+        {
+            inWater = true;
+        }
+        else
+        {
+            inWater = false;
+        }
+    }
+
+    IEnumerator RestoreCooldown()
+    {
+        yield return new WaitForSeconds(2.0f);
+        IncreaseWaterPower();
     }
 }
