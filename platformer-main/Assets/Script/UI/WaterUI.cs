00a3ed23ba5fb50c51907b4c07c9fc2cd345b82d
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-05-16 23:25:25 +0100

    changed WaterUI.cs

--- platformer-main/Assets/Script/UI/WaterUI.cs
+++ platformer-main/Assets/Script/UI/WaterUI.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class WaterUI : MonoBehaviour
 {
+    [SerializeField] private WaterSystem waterSystem;
+    [SerializeField] private GameObject[] waterItems;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        waterSystem.onWaterChange += WaterSystem_onWaterChange;
+
+
+
+        UpdateWaterDisplay();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void WaterSystem_onWaterChange(int gainWater)
     {
-        
+        UpdateWaterDisplay();
     }
+
+
+    void UpdateWaterDisplay()
+    {
+        int waterpower = waterSystem.wp;
+
+        for (int i = 0; i < waterpower; i++)
+        {
+            waterItems[i].SetActive(true);
+        }
+        for (int i = waterpower; i < waterItems.Length; i++)
+        {
+            waterItems[i].SetActive(false);
+        }
+
+    }
+  
 }
