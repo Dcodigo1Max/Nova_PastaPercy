4b8dac5f36a75e0281159fb0f900ddac356df377
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-05-20 00:16:43 +0100

    Menu code work

--- platformer-main/Assets/Script/Menu Scripts/Button Start.cs
+++ platformer-main/Assets/Script/Menu Scripts/Button Start.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class ButtonStart : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void StartGame()
+    public void StartGame()
     {
         SceneManager.LoadScene("Level 3");
     }
