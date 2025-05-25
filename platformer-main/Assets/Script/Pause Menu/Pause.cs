8308dc85355d70e246aa3f749ee8e14809e68e41
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-05-25 13:32:45 +0100

    Pause menu and other adjustments

--- platformer-main/Assets/Script/Pause Menu/Pause.cs
+++ platformer-main/Assets/Script/Pause Menu/Pause.cs
@@ -3,14 +3,52 @@ using UnityEngine;
 public class Pause : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject pauseMenu;
+    public bool paused;
+    
+    
     void Start()
     {
-        
+        pauseMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("p"))
+        {
+            if(paused)
+            {
+                ContinueGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
     }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void ContinueGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+
+    }
+
+
+
+
+
+
+
+
+
 }
